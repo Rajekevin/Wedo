0a420c4712d79cb5295f8a7d53ef1736e51b27e2
Rajekevin <rajekevin@hotmail.fr>
2016-07-31 10:52:18 +0200

    User save in bdd, verif en cours

--- controllers/userController.class.php
+++ controllers/userController.class.php
@@ -17,7 +17,218 @@ class userController{
 
 
 	public function subscribeAction($args) {
+
+		session_start();
+
+
+		 // Afficher les erreurs à l'écran
+	    ini_set('display_errors', 1);
+	    // Enregistrer les erreurs dans un fichier de log
+	    ini_set('log_errors', 1);
+	    // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+	    ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
 		$v = new view();
 		$v->setView("user/subscribe");
+		
+	
+
+	   	$pays = ["fr"=>"France", "gb"=>"England", "es"=>"Spanish", "it"=>"Italy"];
+	    $sexe = [0=>"homme", 1=>"femme"];
+	    $v->assign("pays",$pays);
+	    $v->assign("sexe",$sexe);
+
+	    
+	    $error = FALSE;
+	    $msg_error = "";
+
+
+
+
+	    if( isset($args['login']) &&  isset($args['pass1']) &&  isset($args['sexe'])&&
+		    isset($args['birth']) &&  isset($args['email']) &&    isset($args['pass2']) &&    isset($args['ville'])  )
+	    {
+
+	        $args['login'] = strtolower(trim($args['login']));
+	        $args['email'] = strtolower(trim($args['email']));
+	        $args['ville'] = strtolower(trim($args['ville']));
+
+	        //$args['sexe'] = strtolower(trim($args['sexe']));
+
+	        
+
+	        if(strlen($args['login'])<2)
+	        {
+	            $error = TRUE;
+	            $msg_error .= "Le login doit faire plus d'un caractère";
+	    	}
+	        /*if(strlen($this->surname)<2){
+	            $error = TRUE;
+	            $msg_error .= "<li>Le prénom doit faire plus d'un caractère";
+	        }
+	        if( $this->name === $this->surname){
+	            $error = TRUE;
+	            $msg_error .= "<li>Le prénom doit être différent du nom";
+	        }*/
+	        if(!filter_var($args['email'], FILTER_VALIDATE_EMAIL))
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Email invalide";
+	        }
+	        if(strlen($args['pass1']) <8 || strlen($args['pass1'])>12)
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Le mot de passe doit faire entre 8 et 12 caractères";
+	        }
+	        if($args['pass1'] != $args['pass2'])
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Le mot de passe de confirmation ne correspond pas";
+	        }
+	        /*if(! isset($ville[$args['pays']]) )
+	        {
+	            $error = TRUE;
+	            $msg_error .= "<li>Votre ville n'existe pas";
+	        }*/
+	        /*if(!in_array($this->sexe, $sexe))
+	        {
+	            $error = TRUE;
+	            $msg_error .= "<li>Le genre n'existe pas";
+	        }*/
+
+	        
+	        //gestion du captcha
+
+	       /* $code = strtoupper($code);
+	        if( $code != $captcha ) 
+	        {
+	           $error = TRUE;
+	           $msg_error .= "<li>Le code est incorrect";
+	        }*/
+
+	      
+	        $now = new DateTime();
+
+
+	        //Vérifier la présence du "-"
+	        if( strpos($args['birth'], "-") )
+	        {
+	            $explode_date = explode("-", $args['birth']);
+	            
+	            list($year, $month, $day) = explode('-', $args['birth']);
+	            $time_birthday = mktime(0, 0, 0, $month, $day, $year);
+
+	            if( checkdate ( $month , $day , $year ))
+	            {
+	                $bith = new DateTime($args['birth']);
+	                $interval = $now->diff($bith);
+	                $age = $interval->y;
+
+	                if($age >= 100 )
+	                {
+	                    $error = TRUE;
+	                    $msg_error .= " | Date de naissance incorrecte";
+	                }
+	            }
+	        }
+	        //Vérifier la présence du "/"
+	        else if( strpos($args['birth'], "/") )
+	        {
+
+	            list($day, $month, $year) = explode('/', $args['birth']);
+	            $time_birth = mktime(0, 0, 0, $month, $day, $year);
+
+	            if( checkdate ( $month , $day , $year ))
+	            {
+	                $bithDate = new DateTime($year."-".$month."-".$day);
+	                $interval = $now->diff($bithDate);
+	                $age = $interval->y;
+
+	                if($age >= 100 )
+	                {
+	                    $error = TRUE;
+	                    $msg_error .= " | Date de naissance incorrecte";
+	                }
+	            }
+	        }
+	        //Erreur
+	        else
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Date de naissance incorrecte";
+	        }
+
+	      	if($error)
+	      	{
+	            echo "<ul>";
+	            echo '<script type="text/javascript">window.alert("'.$msg_error.'");</script>';
+	            echo "</ul>";
+	        }
+		    else
+		    {
+	        $pwd = password_hash($args['pass1'], PASSWORD_DEFAULT);
+
+	       	$statut=2; 
+	       	$actif=0;
+	       		//$img="defaut.jpg";
+
+
+			$membre = new membre(); 
+			$membre->setLogin($args['login']);
+			$membre->setSexe($args['sexe']);
+
+	
+
+			$date1 = strtr($args['birth'], '/', '-');
+			$date = date('Y-m-d', strtotime($date1));
+
+
+
+			$membre->setBirth($date);
+
+			
+
+	
+				
+			$membre->setPass($pwd);
+			// $membre->setVille($args['ville']);
+			$membre->setDateInscription(date('Y-m-d H:i:s'));
+			$membre->setActif($actif);
+			$membre->setMail($args['email']);
+
+			$membre->setVille($args['ville']);
+			$membre->setStatut($statut);
+
+			
+			// $membre->setAvatar($img);
+			$membre->setToken();
+
+			$membre->save();
+
+
+			//on récupère l'id de l'utilisateur
+
+   //       	$thisuser = new membre();
+			// $tab = $thisuser->getOneBy(['mail'=>$args['email']]);
+   //       	//envoie de l'email de validation
+   //          $email = new email();			
+			// //On renseigne le sujet et le message de l'email
+			// $email->setSujet("Validation de votre inscription");
+			// $email->setDestinataires($args['email']);
+			// $email->setMessage("Bienvenue sur WEDO,<br />
+			// Pour activer votre compte, veuillez cliquer sur le lien ci dessous ou bien copier celui-ci dans l'url de votre navigateur. 
+			// <br />
+
+			// ".LINK."index/activerCompte?id=".$tab['id']."<br />
+
+			// ---------------<br />
+			// Ceci est un mail automatique, Merci de ne pas y répondre");			
+			// //envoie de l'email
+			// $email->envoieEmail();
+   //      	echo 'Bienvenue '.$args['login'].', vous allez recevoir un email pour valider votre compte';
+   //      	var_dump($tab['id']);
+
+			}
+		
+		}
 	}
 }
--- core/validator.class.php
+++ core/validator.class.php
@@ -1,8 +1,5 @@
 <?php
-class validator{
-
-
-
+class Validator extends basesql{
 	public function __construct(){
 
 	}
@@ -10,38 +7,160 @@ class validator{
 	public static function check($struct, $data){
 		$listErrors = [];
 		foreach ($struct as $name => $options) {
-
-			if($options["required"] && self::isEmpty($data[$name])){
+			if($options["required"] && $options["type"]!="file" && self::isEmpty($data[$name])){
 				$listErrors[]=$options["msgerror"];
 			}
-			elseif($options["type"]=="password" && !self::passwordCorrect($data[$name])) {
+			else if($options["required"] && $options["type"]=="file" && self::isFileEmpty($_FILES[$name])){
 				$listErrors[]=$options["msgerror"];
 			}
-			
+			elseif($options["msgerror"]=="password" && !self::passwordCorrect($data[$name])) {
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="confirm_password" && !self::passwordCorrect($data[$name]) && self::passwordIdentical($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="new_email" && !self::newEmailCorrect($data[$name])) {
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="email" && !self::emailCorrect($data[$name])) {
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="new_username" && self::existUsername($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"] == "username_doesnt_exists" && !self::existUsername($data[$name])) {
+				$listErrors[] = $options["msgerror"];
+			}
+			elseif($options["msgerror"]=="teamName" && !self::existTeamName($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="emailOrUsername" && !self::verifUsernameOrEmail($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"] && $options["msgerror"]=="email_exist" && !self::emailExist($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["msgerror"]=="new_teamName" && !self::newExistTeamName($data[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif($options["required"] && $options["msgerror"]=="avatar"  && $options["type"]=="file"&& !self::verifAvatar($_FILES[$name])){
+				$listErrors[]=$options["msgerror"];
+			}
+			elseif ($options["required"] && $options["msgerror"]=="date_format"  && !self::dateFormat($data[$name])) {
+				$listErrors[] = $options["msgerror"];
+			}
+			elseif ($options["required"] && $options["msgerror"]=="time_format"  && !self::timeFormat($data[$name])) {
+				$listErrors[] = $options["msgerror"];
+			}
 		}
-
 		return $listErrors;
 	}
 
+	public static function dateFormat($date) {
+		if (date_parse_from_format('d/m/Y', $date)["error_count"] != 0) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+
+	public static function timeFormat($time) {
+		if (date_parse_from_format("H:i", $time)["error_count"] != 0) {
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
+
+	public static function verifAvatar($var){
+		$sizeMax = 2097152;
+		$extension = ["jpg","jpeg","gif","png"];
+		$extensionUpload = strtolower(substr(strrchr($var['name'], '.'), 1));
+		if($var['size'] > $sizeMax || $var['size'] == 0){
+			return !($var['size'] <= $sizeMax || $var['size'] == 0);
+		}else if(!in_array($extensionUpload, $extension)){
+			return !(in_array($extensionUpload, $extension));
+		}else{
+			return TRUE;
+		}
+	}
+
+	//fonction de verif pour l'invitation par email ou username
+	public static function verifUsernameOrEmail($var){
+		if(!self::emailCorrect($var)){//Si email
+			$idUser = User::findBy("email",$var,"string");
+			//premiere partie de la requete : on verifie que l'utilisateur n'est pas deja dans la team, ou si il n'est pas deja invité
+			if($idUser){
+				return !(TeamHasUser::findBy(["idUser","idTeam"],[$idUser[0]->id,$_SESSION['idTeam']],["int","int"]) || (Invitation::findBy(["idTeamInviting","idUserInvited"],[$_SESSION['idTeam'],$idUser[0]->id],["int","int"])));
+			}else{
+				return false;
+			}
+		}else{//Sinon pseudo
+			$idUser = User::findBy("username",$var,"string");
+			if($idUser){
+				return !(TeamHasUser::findBy(["idUser","idTeam"],[$idUser[0]->id,$_SESSION['idTeam']],["int","int"]) || (Invitation::findBy(["idTeamInviting","idUserInvited"],[$_SESSION['idTeam'],$idUser[0]->id],["int","int"])));
+			}else{
+				return false;
+			}
+		}
+	}
+
+	public static function emailExist($email) {
+		return (User::findBy("email", $email, "string"));
+	}
+
+	public static function isFileEmpty($var){
+		return empty($var);
+	}
 
 	public static function isEmpty($var){
-		return trim($var)==false;
+		return empty(trim($var));
+	}
 
-		
+	public static function passwordIdentical($var){
+		return !(strcmp($var, $_SESSION['passwordValidator']));
 	}
 
 	public static function passwordCorrect($var){
+		$_SESSION['passwordValidator'] = $var;
+// on stocke le mdp avant de vérifier qu'il soit identique au mot de passe de conf,
+// pas d'inquietude, la session est detruite a la fin du processus d'activation
 		return !( strlen($var)<8 || strlen($var)>12 ||
 					!preg_match("/[0-9]/", $var) ||
 					!preg_match("/[a-z]/", $var) ||
-					!preg_match("/[A-Z]/", $var) );	
+					!preg_match("/[A-Z]/", $var) );
 	}
 
+	public static function emailCorrect($var){
+		return !(filter_var($var,FILTER_VALIDATE_EMAIL));
+	}
+
+	public static function newEmailCorrect($var){
+		return !((filter_var($var,FILTER_VALIDATE_EMAIL)) && (User::findBy("email", $var, "string")) );
+	}
+
+	public static function existUsername($var){
+		return (User::findBy("username",$var,"string"));
+	}
+
+	public static function newExistTeamName($var){
+		if(!self::notChangingTeamName($var)){
+			return !(strlen($var)<4 || strlen($var)>30);
+		}
+	}
+
+	public static function existTeamName($var){
+		return !((strlen($var)<4 || strlen($var)>30) || (Team::findBy("teamName",$var,"string")));
+	}
+
+	public static function notChangingTeamName($var){
+		$nameTeam = Team::findById($_SESSION['temp_idTeam']);
+		//Si l'utilisateur ne change pas son nom d'equipe on update tout de meme les autres champs
+		if(strcmp($nameTeam->teamName, $var) != 0){
+			return !strcmp($nameTeam->teamName, $var);
+		}
+	}
 }
-
-
-
-
 
 
 
