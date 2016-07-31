84d12f6e6fd2bef530851de339d92b2203eeb669
Rajekevin <rajekevin@hotmail.fr>
2016-07-31 18:58:05 +0200

    Created categorie class

---
+++ models/categorie.class.php
@@ -0,0 +1,51 @@
+<?php
+class categorie extends basesql{
+	protected $id;
+	protected $name;
+	protected $description;
+	protected $image;
+	//SETTERS
+	public function setId($id){
+		$this->id=$id;
+	}
+	public function setName($name){
+		$this->name=trim($name);
+	}
+	public function setDescription($description){
+		$this->description=trim($description);
+	}
+	public function setImage($image){
+		$this->image=$image;
+	}
+	//GETTERS
+	public function getId($id){
+		return $this->$id;
+	}
+	public function getName($Name){
+		return $this->Name;
+	}
+	public function getDescription($description){
+		return $this->description;
+	}
+	public function getImage($image){
+		return $this->$image;
+	}
+	public function getForm(){
+		return [	
+					"options" => [
+						"method"=>"POST",
+						"action"=>"",
+						"id"=>"contact",
+						"name"=>"contact",
+						"submit"=>"Envoyer"
+						],
+					"struct" => [
+						"name"=>[ "label"=>"", "type"=>"text", "id"=>"name", "placeholder"=>"Votre nom", "required"=>"Votre nom", "msgerror"=>"name" ],
+						"firstname"=>[ "label"=>"", "type"=>"text", "id"=>"firstname", "placeholder"=>"Votre prénom", "required"=>1, "msgerror"=>"firstname" ],
+						"email"=>[ "label"=>"", "type"=>"text", "id"=>"email", "placeholder"=>"Votre email", "required"=>1, "msgerror"=>"passwordconfirm" ],
+						"content"=>[ "label"=>"", "type"=>"textarea", "id"=>"content", "placeholder"=>"Votre message", "required"=>1, "msgerror"=>"contentpage" ]
+					]
+		];
+	}
+	
+}
