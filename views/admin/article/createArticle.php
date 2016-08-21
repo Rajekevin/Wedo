1d6b3523338a9caef503e31fd3ab846ea1f3f010
Rajekevin <rajekevin@hotmail.fr>
2016-08-21 09:42:26 +0200

    ok

--- views/admin/article/createArticle.php
+++ views/admin/article/createArticle.php
@@ -93,11 +93,11 @@ if(isset($_POST['valider'])&& isset($_POST['title'])&&isset($_POST['description'
 
 			
 			/*on recherche l'id de la catégorie sélectionné par l'user */
-			$idCategory = categorie::findBy("name", $_POST['categorie'], "string");
-			$idCategory = $idCategory->getId();
+			// $idCategory = categorie::findBy("name", $_POST['categorie'], "string");
+			// $idCategory = $idCategory->getId();
 		
 		
-			$a->setIdCategory($idCategory);
+			// $a->setIdCategory($idCategory);
 
 			if(upload("img","public/img/article/", array("png","jpg","gif", "bmp"),100000000000000000000,array(15420,15420))==true)
 			{
