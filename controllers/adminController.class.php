00bef0cb7d5ad4c5815ef914102caad64455428c
Rajekevin <rajekevin@hotmail.fr>
2016-08-24 13:50:22 +0200

    testing

--- controllers/adminController.class.php
+++ controllers/adminController.class.php
@@ -383,6 +383,14 @@ class adminController{
 		public function removeCategorieAction($args){
 		session_start();
 
+
+		    // Afficher les erreurs à l'écran
+	    ini_set('display_errors', 1);
+	    // Enregistrer les erreurs dans un fichier de log
+	    ini_set('log_errors', 1);
+	    // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+	    ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+
 	 	if (isset($_SESSION['login'])&&($_SESSION['statut'])==1)
 	            { 
 		$v = new view();
