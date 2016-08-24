deb28c64dd4fe659b25f9fc9fed9f51f2436fe25
Rajekevin <rajekevin@hotmail.fr>
2016-08-24 15:21:09 +0200

    categorisation des articles x)

--- views/article/articlelist.php
+++ views/article/articlelist.php
@@ -4,48 +4,49 @@
   
 
      <div class="content">
+
+   
+
     
 
-<?php $title = $value['name'];  ?>
-
      <?php
-    $c = new categorie();
-    $a = new article();
-        //On cherche tous les catégories
-        $categorie = $c->getAllBy([],['id'=>'ASC'],'');
 
-        
+        $a = new article();       
 
         $idCategorie = Article::findBy("id_category",$value['id'],"int");
        
         //on récupère tous les 3 derniers articles en fonction de chaque catégorie
-        $articles = $a->getAllBy(['id_category' => $value['id']], ['id' => 'DESC'] ,'');
+        $articles = $a->getAllBy(['id_category' => $value['id']], ['id' => 'DESC'] ,[]);
 
         ?>
-
-         
-      <?php foreach ($articles as $key => $value): ?>
-
-
-
-            <div class="article_list"> 
+     <div class="article_list"> 
 
                 <div class="article">
-                    <center><i><h2><?= $title; ?></h2></i> </center> 
+                    <center><i><h2><?= $value['name']; ?></h2></i> </center> 
 
-                    <div class="logo_article"><img src="<?= WEBROOT; ?>public/img/article/<?= $value['img']; ?>" /></div> 
-                    <a href="<?= ARTICLE.$value['title'] ?>">
-                    <img class="top" src="<?= WEBROOT; ?>public/img/article/<?= $value['img']; ?>" />
+                    <p><?php 
+                    $nbArticles= count($articles);
+                    if($nbArticles > 1) {
+                      print_r($nbArticles." articles");
+                    }else{
+                      print_r("pas d'article");
+                    }?>
+          
+                    </p>
+                    <a href="<?= ARTICLE.$value['name'] ?>">
+                    <div class="divImageZoom">
+                    
+                    </div>
                     </a>
-                    <h3></h3><p></p>
-                    <a class="Wedo_Button" href="">Je commente</a>
+                    
+                    <a class="Wedo_Button" href="">Je découvre les articles ..</a>
           </div>
 
 
          </div>
+  
 
 
-         <?php endforeach; ?>
 
 
      
