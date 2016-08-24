393294333c719be0a4e3caecc131e12a9ad14b51
Rajekevin <rajekevin@hotmail.fr>
2016-08-24 16:18:50 +0200

    testing

--- controllers/articleController.class.php
+++ controllers/articleController.class.php
@@ -228,6 +228,15 @@ class articleController
 
 		public function allAction($args){
 		session_start();
+
+		    
+	    // Afficher les erreurs à l'écran
+	    ini_set('display_errors', 1);
+	    // Enregistrer les erreurs dans un fichier de log
+	    ini_set('log_errors', 1);
+	    // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+	    ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+	    
 		$v = new view();
 		
 		$var = implode ($args);
@@ -255,6 +264,9 @@ class articleController
 	
 		
 	}
+
+
+
 	public function ratingAction($args){
 		session_start();
 		$v = new view();
