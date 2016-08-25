b0dd571412aea119a012d7c1dde27342bb2bcd73
Rajekevin <rajekevin@hotmail.fr>
2016-08-25 10:33:55 +0200

    testing

--- controllers/adminController.class.php
+++ controllers/adminController.class.php
@@ -11,6 +11,15 @@ class adminController{
 	public function indexAction($args){
 		session_start();
 
+		    
+	    // Afficher les erreurs à l'écran
+	    ini_set('display_errors', 1);
+	    // Enregistrer les erreurs dans un fichier de log
+	    ini_set('log_errors', 1);
+	    // Nom du fichier qui enregistre les logs (attention aux droits à l'écriture)
+	    ini_set('error_log', dirname(__file__) . '/log_error_php.txt');
+	    
+
 		echo"Vous n'avez pas accès à cette partie";
 
 	if(isset($_SESSION['id']) && isset($_SESSION['token']) && isset($_SESSION['statut']) && isset($_SESSION['login'])){
