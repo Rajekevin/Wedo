f14c5c77de361fcb04282996244beb6b4e2463e6
Rajekevin <rajekevin@hotmail.fr>
2016-08-26 16:41:45 +0200

    testing

--- controllers/articleController.class.php
+++ controllers/articleController.class.php
@@ -32,7 +32,7 @@ class articleController
 		
 	
 		$a = new article();
-		var_dump($a);
+		var_dump($args[0]);
 		$url = article::findBy("url", $args[0], "string");
 		var_dump($url);		
 		if($url==false)
