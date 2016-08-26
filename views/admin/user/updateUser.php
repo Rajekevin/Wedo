6931d572de80566f242191d322d57530084ad9da
Rajekevin <rajekevin@hotmail.fr>
2016-08-26 14:53:44 +0200

    updateUser for admin

--- views/admin/user/updateUser.php
+++ views/admin/user/updateUser.php
@@ -11,12 +11,33 @@
 
 	$id=$membre->setId($tab['id']);
 
-	if(isset($_POST['valider'])&& isset($_POST['login'])&&isset($_POST['mail'])&&isset($_POST['statut']))
+if(isset($_POST['valider'])&& isset($_POST['login'])&&isset($_POST['mail'])&&isset($_POST['statut'])&&isset($_POST['pass1']) &&isset($_POST['pass2']) )
 	{
 	 			
 		$membre->setLogin($_POST['login']);
 		$membre->setMail($_POST['mail']);			
 		$membre->setStatut($_POST['statut']);
+
+
+		if(strlen($_POST['pass1']) <8 || strlen($_POST['pass2'])>12)
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Le mot de passe doit faire entre 8 et 12 caractères";
+	            echo $msg_error;
+	        }
+		  if($_POST['pass1'] != $_POST['pass2'])
+	        {
+	            $error = TRUE;
+	            $msg_error .= " | Le mot de passe de confirmation ne correspond pas";
+	            echo $msg_error;
+	        }
+
+	        $pwd = password_hash($_POST['pass1'], PASSWORD_DEFAULT);
+
+
+	     $membre->setPass($pwd);
+
+
 		$membre->save();
 	}
 ?>
@@ -24,6 +45,15 @@
 	<label>
 		<h5>LOGIN</h5> 
 		<input type="text" name="login" value="<?php echo $tab['login']; ?>"/>
+	</label>
+
+	<label>
+		<h5>Nouveau mot de passe : </h5> 
+		<input type="password" name="pass1" id="name"  placeholder="Mot de passe" required/>
+	</label>
+	<label>
+		<h5>Retaper le mot de passe : </h5> 
+		 <input type="password" name="pass2"  id="name" placeholder="Retapez votre mot de passe" required/>
 	</label>
 
 	<label>
